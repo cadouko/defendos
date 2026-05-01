fadb1bd4b92cae5aec909456c86f2941f6a95917
cadouko <cadouko@users.noreply.github.com>
2026-05-01 21:29:32 +0000

    fix: use UEFI-only boot mode and add grub to CI container

--- iso/defendos/profiledef.sh
+++ iso/defendos/profiledef.sh
@@ -9,9 +9,7 @@ iso_application="DefendOS – Cybersecurity Live OS"
 iso_version="1.0.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-x64.grub.eltorito')
+bootmodes=('uefi-x64.grub.esp')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
